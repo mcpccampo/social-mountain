11e7a86935e03769d63c4c1cbd48d7dc6afa702e
Camilo Campo <mcp.ccampo@gmail.com>
2021-01-12 20:31:29 -0500

    review

--- src/components/App.js
+++ src/components/App.js
@@ -5,34 +5,35 @@ import './App.css';
 import Header from './Header/Header';
 import Compose from './Compose/Compose';
 
+import axios from 'axios';
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      posts: []
+      posts: [],
+      uri: 'https://practiceapi.devmountain.com/api',
     };
 
-    this.updatePost = this.updatePost.bind( this );
-    this.deletePost = this.deletePost.bind( this );
-    this.createPost = this.createPost.bind( this );
+    this.updatePost = this.updatePost.bind(this);
+    this.deletePost = this.deletePost.bind(this);
+    this.createPost = this.createPost.bind(this);
   }
-  
+
   componentDidMount() {
-
+    axios.get(`https://practiceapi.devmountain.com/api/posts`).then((res) => {
+      this.setState({
+        posts: res.data,
+      });
+    });
   }
 
-  updatePost() {
-  
-  }
+  updatePost() {}
 
-  deletePost() {
+  deletePost() {}
 
-  }
-
-  createPost() {
-
-  }
+  createPost() {}
 
   render() {
     const { posts } = this.state;
@@ -42,9 +43,7 @@ class App extends Component {
         <Header />
 
         <section className="App__content">
-
           <Compose />
-          
         </section>
       </div>
     );
